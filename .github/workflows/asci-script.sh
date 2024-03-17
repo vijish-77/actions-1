54022bf086c50cbbfd3b28883113bb2d211eee93
vijish-77 <163699549+vijish-77@users.noreply.github.com>
2024-03-17 13:41:44 +0530

    added new changes 1

--- .github/workflows/asci-script.sh
+++ .github/workflows/asci-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, I am a DRAGON ...RAWR" >> dragon.txt
+cowsay -f dragon "Run for cover, I am a DRAGON ....RAWR" file >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
